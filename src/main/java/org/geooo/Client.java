b3d5b11592429a8f2f6de829cbcce32bb12dcef9
Georg Friedrich <gentus72@gmail.com>
2025-06-12 00:30:10 +0200

    mini comments + exit, safety commit

--- src/main/java/org/geooo/Client.java
+++ src/main/java/org/geooo/Client.java
@@ -61,10 +61,16 @@ public final class Client extends ClientDTO {
             this.clientFile.writeToFile(this);
         }
 
+        // Connect all commands to an according function
+        // Makes development and readability easy
         registeredClientCommands.put(ClientCommand.CONNECT, this::handleClientCommandCONNECT);
         registeredClientCommands.put(ClientCommand.INFO, this::handleClientCommandINFO);
         registeredClientCommands.put(ClientCommand.AUTOGET, this::handleClientCommandAUTOGET);
         registeredClientCommands.put(ClientCommand.AUTOUPLOAD, this::handleClientCommandAUTOUPLOAD);
+        registeredClientCommands.put(ClientCommand.EXIT, this::handleClientCommandEXIT);
+
+        // Servercommands don't have to be registered, because they get interpreted by the server
+        // -> leading to a Serverresponse (which is handled by the client)
 
         registeredServerResponses.put(ServerResponse.INFO, this::handleServerResponseINFO);
         registeredServerResponses.put(ServerResponse.REDIRECT, this::handleServerResponseREDIRECT);
@@ -141,6 +147,9 @@ public final class Client extends ClientDTO {
         }
     }
 
+    // REDIRECT <destinationAddress>
+    // Response to any command thats not supported by a normal server
+    // The connection will automatically switch to the CCServer and send the original command
     public void handleServerResponseREDIRECT(String[] args) {
         HOST_ADDRESS = args[1];
         Logger.info(String.format("Being redirected to: %s", args[1]));
@@ -151,6 +160,10 @@ public final class Client extends ClientDTO {
         handleServerInteraction(currentClientInput);
     }
 
+    // INFO <RESSOURCE | NETWORK> <? ressourceUUID>
+    // Response to the INFO command
+    // Gives information about the network or a specific ressource
+    // If INFO was called on a non-CCServer it will redirect to the CCServer
     public void handleServerResponseINFO(String[] args) {
         switch (args[1]) {
             case "NETWORK" -> {
@@ -165,6 +178,9 @@ public final class Client extends ClientDTO {
         }
     }
 
+    // DOWNLOAD <ressourceUUID> <blockUUID>
+    // Response to the GETBLOCK command
+    // Is followed by the block file
     public void handleServerResponseDOWNLOAD(String[] args) {
         try {
             // create directory
@@ -186,8 +202,9 @@ public final class Client extends ClientDTO {
     }
 
     // AUTH <SUCCESS | FAIL>
-    // Response to the AUTH command
-    // signals, whether the client can upload or not
+    // Response to the AUTH (authorizing a ressource) command
+    // Signals, whether the client can upload to the network or not
+    // Is followed by an exchange of ressourcefiles
     public void handleServerResponseAUTH(String[] args) {
         if (args[1].equals("SUCCESS")) {
             Logger.success("Upload authorization granted! Sending ressourcefile...");
@@ -212,8 +229,10 @@ public final class Client extends ClientDTO {
         }
     }
 
-    // success response for AUTH to a hostServer
     // SUCCESS <ressourceUUID> <decryptedBlockUUID>
+    // Response for AUTH (authorizing a block) to a normal server
+    // Means the server could verify the clients public key
+    // Is followed by the client uploading the according block
     public void handleServerResponseSUCCESS(String[] args) {
         File blockFile = new File(String.format("%s%s/%s.g2gblock", RESSOURCE_DIRECTORY, args[1], args[2]));
         if (!blockFile.exists()) {
@@ -235,6 +254,8 @@ public final class Client extends ClientDTO {
         }
     }
 
+    // CONNECT <address>
+    // Tries to establish a connection to the given address
     public void handleClientCommandCONNECT(String[] args) {
         HOST_ADDRESS = args[1];
         HOST_PORT = 7000;
@@ -256,6 +277,8 @@ public final class Client extends ClientDTO {
         }
     }
 
+    // INFO <NETWORK | RESSOURCE> <networkUUID | ressourceUUID | ALL>
+    // Gives information about all or a specific network / ressource
     public void handleClientCommandINFO(String[] args) {
         switch (args[1]) {
             case "NETWORK" -> {
@@ -324,6 +347,10 @@ public final class Client extends ClientDTO {
         }
     }
 
+    // AUTOGET <ressourceUUID>
+    // Assembles GETBLOCK commands from the ressourcefile and
+    // sends the command to the according server where its
+    // hosted - one after another
     public void handleClientCommandAUTOGET(String[] args) {
         RessourceFile ressourceFile = new RessourceFile(RESSOURCE_DIRECTORY + args[1] + ".g2g");
 
@@ -351,6 +378,10 @@ public final class Client extends ClientDTO {
         Logger.success("All blocks downloaded!");
     }
 
+    // AUTOUPLOAD <ressourceUUID>
+    // Assembles AUTH commands from the ressourcefile and
+    // sends the command to the according server where its
+    // hosted - one after another
     public void handleClientCommandAUTOUPLOAD(String[] args) {
         RessourceFile ressourceFile = new RessourceFile(RESSOURCE_DIRECTORY + args[1] + ".g2g");
 
@@ -377,6 +408,13 @@ public final class Client extends ClientDTO {
 
         Logger.success(String.format("[%d/%d] blocks uploaded!", commands.size(), commands.size()));
         Logger.success("Ressource uploaded successfully!");
+    }
+
+    // EXIT
+    // Simply shuts the client down relatively gracefully
+    public void handleClientCommandEXIT(String[] args) {
+        Logger.info("Shutting down...");
+        System.exit(0);
     }
 
     public void disconnect() {
@@ -406,3 +444,4 @@ public final class Client extends ClientDTO {
         }
     }
 }
+
